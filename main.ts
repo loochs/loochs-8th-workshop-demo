f1004f67e827c8f0f8e1df05bc48d3a7118aa9e5
Loochs <54494160+loochs@users.noreply.github.com>
2020-06-14 16:51:55 +0900

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once)
+})
 basic.forever(function () {
-	
+    for (let index = 0; index < 4; index++) {
+        basic.showIcon(IconNames.SmallHeart)
+        basic.showIcon(IconNames.Heart)
+    }
 })
